8ff517076eb58aecabe16a03c6cfa06e29e4b286
yopaseopor <yopaseopor@gmail.com>
2025-07-08 01:38:36 +0200

    Update myCustomLayer.js

--- src/layers/myCustomLayer.js
+++ src/layers/myCustomLayer.js
@@ -21,21 +21,6 @@ export const myCustomLayer = [
             fill: new ol.style.Fill({ color: 'rgba(200, 200, 200, 0.5)' }),
             stroke: new ol.style.Stroke({ color: '#3399CC', width: 1.25 })
         })
-        }),
-    new ol.layer.Tile({
-        title: 'ICGC - Ortofoto de Catalunya Provisional',
-        source: new ol.source.TileWMS({
-            attributions: 'Institut Cartogràfic i Geològic de Catalunya',
-            url: 'http://geoserveis.icgc.cat/servei/catalunya/orto-territorial/wms',
-            params: {
-                'LAYERS': 'ortofoto_color_provisional',
-                'VERSION': '1.3.0',
-                'FORMAT': 'image/jpeg',
-                'TRANSPARENT': false'
-            },
-            serverType: 'mapserver'
-        }),
-        visible: false
     }),
     // OSM Tile layer
     new ol.layer.Tile({
