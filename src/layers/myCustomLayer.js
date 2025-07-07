7cb849adec8fe7aee835b0d3ab1d137fd94f6265
yopaseopor <yopaseopor@gmail.com>
2025-07-07 02:08:34 +0200

    test external layers

--- src/layers/myCustomLayer.js
+++ src/layers/myCustomLayer.js
@@ -1,31 +1,36 @@
 // Example: Custom OpenLayers Tile Layer
 export const myCustomLayer = [
-    // Example: MapTiler Openmaptiles (from config.js)
-    new ol.layer.VectorTile({
-        title: 'MapTiler Openmaptiles',
-        visible: false,
-        opacity: 1.0,
-        source: new ol.source.VectorTile({
-            tilePixelRatio: 1,
-            tileGrid: ol.tilegrid.createXYZ({ minZoom: 0, maxZoom: 22 }),
-            format: new ol.format.MVT(),
-            url: 'https://api.maptiler.com/tiles/v3-openmaptiles/{z}/{x}/{y}.pbf?key=Faz9gJu55zrWejNF55oZ',
-            attributions: [
-                '<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>',
-                '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
-            ],
-            crossOrigin: 'anonymous',
-            projection: 'EPSG:3857'
-        }),
-        style: new ol.style.Style({
-            fill: new ol.style.Fill({ color: 'rgba(200, 200, 200, 0.5)' }),
-            stroke: new ol.style.Stroke({ color: '#3399CC', width: 1.25 })
-        })
-    }),
-    // Example: OSM Layer (from before)
-    new ol.layer.Tile({
-        title: 'Custom OSM Layer',
-        visible: false,
-        source: new ol.source.OSM(),
+    new ol.layer.Group({
+        title: 'myCustomLayer',
+        layers: new ol.Collection([
+            // MapTiler Openmaptiles
+            new ol.layer.VectorTile({
+                title: 'MapTiler Openmaptiles',
+                visible: false,
+                opacity: 1.0,
+                source: new ol.source.VectorTile({
+                    tilePixelRatio: 1,
+                    tileGrid: ol.tilegrid.createXYZ({ minZoom: 0, maxZoom: 22 }),
+                    format: new ol.format.MVT(),
+                    url: 'https://api.maptiler.com/tiles/v3-openmaptiles/{z}/{x}/{y}.pbf?key=Faz9gJu55zrWejNF55oZ',
+                    attributions: [
+                        '<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>',
+                        '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
+                    ],
+                    crossOrigin: 'anonymous',
+                    projection: 'EPSG:3857'
+                }),
+                style: new ol.style.Style({
+                    fill: new ol.style.Fill({ color: 'rgba(200, 200, 200, 0.5)' }),
+                    stroke: new ol.style.Stroke({ color: '#3399CC', width: 1.25 })
+                })
+            }),
+            // OSM Tile layer
+            new ol.layer.Tile({
+                title: 'Custom OSM Layer',
+                visible: false,
+                source: new ol.source.OSM(),
+            })
+        ])
     })
 ];
--- src/layers/osmBrightLayer.js
+++ src/layers/osmBrightLayer.js
@@ -1,30 +1,35 @@
 // Example: OSM Bright Layer
 export const osmBrightLayer = [
-    // Example: Versatiles colorful (from config.js)
-    (function() {
-        const colorfulLayer = new ol.layer.VectorTile({
-            title: 'Versatiles colorful',
-            visible: true,
-            opacity: 1.0,
-            source: new ol.source.VectorTile({
-                tilePixelRatio: 1,
-                tileGrid: ol.tilegrid.createXYZ({ minZoom: 0, maxZoom: 14 }),
-                format: new ol.format.MVT(),
-                url: 'https://tiles.versatiles.org/tiles/osm/{z}/{x}/{y}',
-                attributions: [
-                    '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
-                ]
-            }),
-            declutter: true
-        });
-        return colorfulLayer;
-    })(),
-    // Example: OSM Bright (XYZ)
-    new ol.layer.Tile({
-        title: 'OSM Bright',
-        visible: false,
-        source: new ol.source.OSM({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        })
+    new ol.layer.Group({
+        title: 'osmBrightLayer',
+        layers: new ol.Collection([
+            // Versatiles colorful
+            (function() {
+                const colorfulLayer = new ol.layer.VectorTile({
+                    title: 'Versatiles colorful',
+                    visible: true,
+                    opacity: 1.0,
+                    source: new ol.source.VectorTile({
+                        tilePixelRatio: 1,
+                        tileGrid: ol.tilegrid.createXYZ({ minZoom: 0, maxZoom: 14 }),
+                        format: new ol.format.MVT(),
+                        url: 'https://tiles.versatiles.org/tiles/osm/{z}/{x}/{y}',
+                        attributions: [
+                            '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
+                        ]
+                    }),
+                    declutter: true
+                });
+                return colorfulLayer;
+            })(),
+            // OSM Bright (XYZ)
+            new ol.layer.Tile({
+                title: 'OSM Bright',
+                visible: false,
+                source: new ol.source.OSM({
+                    url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                })
+            })
+        ])
     })
 ];
