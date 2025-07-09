bf1b01057a676d0202207ddd8aa4ec2e08e56847
yopaseopor <yopaseopor@gmail.com>
2025-07-10 00:56:31 +0200

    Update index.js

--- src/layers/index.js
+++ src/layers/index.js
@@ -7,4 +7,4 @@ import { myCustomLayer } from './myCustomLayer.js';
 import { osmBrightLayer } from './osmBrightLayer.js';
 import { stamenTonerLayer } from './stamenTonerLayer.js';
 
-export const allLayers = { fr_wms_layers, myCustomLayer, osmBrightLayer, stamenTonerLayer };
+export const allLayers = { FRlayers, myCustomLayer, osmBrightLayer, stamenTonerLayer };
