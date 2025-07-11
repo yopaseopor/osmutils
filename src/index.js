76437e6b2ede1f7e26506c2ad20a0ccf19e91c6f
yopaseopor <yopaseopor@gmail.com>
2025-07-12 01:14:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -842,7 +842,7 @@ $(function () {
 	// Info Control
 	var infoControlBuild = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton').html($('<button type="button"><i class="fa fa-info-circle"></i></button>').on('click', function () {
-			window.location.href = 'https://github.com/yopaseopor/osmpoismap';
+			window.location.href = 'https://github.com/yopaseopor/osmutils';
 		}));
 		return container[0];
 	};
