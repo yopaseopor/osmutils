5f6e54407b9dafef7c8562febd0df8a418069689
yopaseopor <yopaseopor@gmail.com>
2025-07-11 20:49:53 +0200

    Update tms_layers_TW.js

--- src/layers/generated/tms_layers_TW.js
+++ src/layers/generated/tms_layers_TW.js
@@ -37,4 +37,4 @@ export const TW_TMS_LAYERS = [
 if (typeof window.layers === 'undefined') {
     window.layers = [];
 }
-window.layers = window.layers.concat(TW_TMS_LAYERS);*/*/
+window.layers = window.layers.concat(TW_TMS_LAYERS);*/
