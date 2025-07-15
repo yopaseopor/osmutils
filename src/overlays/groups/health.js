cf0e6cb4007cfc44750593072f591965bf4f6998
yopaseopor <yopaseopor@gmail.com>
2025-07-15 14:41:45 +0200

    Update health.js

--- src/overlays/groups/health.js
+++ src/overlays/groups/health.js
@@ -5,37 +5,187 @@ export function healthOverlays() {
         {
         group: getTranslation('health'),
         title: getTranslation('pharmacies'),
-        query: "[out:json][timeout:25];(nwr[\"amenity\"=\"pharmacy\"]({{bbox}}););out body;>;out skel qt;",
-        iconSrc: "https://commons.wikimedia.org/wiki/Special:FilePath/Green_Pharmacy_Cross_w_Bowl_of_Hygieia.svg",
-        iconStyle: 'background-color:rgba(255,255,255,0.4)',
-        style: function (feature) {
-            var key_regex = /^name$/;
-            var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name";
-            var name = feature.get(name_key) || '';
-            var fill = new ol.style.Fill({
-                color: 'rgba(0,255,0,0.4)'
-            });
-            var stroke = new ol.style.Stroke({
-                color: 'rgba(0,255,0,1)',
-                width: 1
-            });
-            var style = new ol.style.Style({
-                image: new ol.style.Icon({
-                    src: "https://commons.wikimedia.org/wiki/Special:FilePath/Green_Pharmacy_Cross_w_Bowl_of_Hygieia.svg",
-                    scale: 0.10
+    query: "[out:json][timeout:25];(nwr[\"amenity\"=\"pharmacy\"]({{bbox}}););out body;>;out skel qt;",
+    iconSrc: "https://commons.wikimedia.org/wiki/Special:FilePath/Green_Pharmacy_Cross_w_Bowl_of_Hygieia.svg",
+    iconStyle: 'background-color:rgba(255,255,255,0.4)',
+    style: function (feature) {
+        var key_regex = /^name$/;
+        var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name";
+        var name = feature.get(name_key) || '';
+        var fill = new ol.style.Fill({
+            color: 'rgba(0,255,0,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(0,255,0,1)',
+            width: 1
+        });
+        var style = new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "https://commons.wikimedia.org/wiki/Special:FilePath/Green_Pharmacy_Cross_w_Bowl_of_Hygieia.svg",
+                scale: 0.10
+            }),
+            text: new ol.style.Text({
+                text: name,
+                offsetX: 7,
+                offsetY: -12,
+                fill: new ol.style.Fill({
+                    color: 'rgba(0,0,0,1)'
                 }),
-                text: new ol.style.Text({
-                    text: name,
-                    offsetX: 7,
-                    offsetY: -12,
-                    fill: new ol.style.Fill({
-                        color: 'rgba(0,0,0,1)'
-                    }),
-                }),
-                fill: fill,
-                stroke: stroke
-            });
-            return style;
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+        return style;
+    }
+},
+{
+    group: getTranslation('hospital'),
+    title: getTranslation('hospital'),
+    query: "[out:json][timeout:25];(nwr[\"amenity\"=\"hospital\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var fill = new ol.style.Fill({
+            color: 'rgba(0,255,0,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(0,255,0,1)',
+            width: 1
+        });
+        return new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale: 0.0200
+            }),
+            text: new ol.style.Text({
+                text: feature.get("name") || '',
+                offsetX: 7,
+                offsetY: -12,
+                fill: new ol.style.Fill({ color: 'rgba(0,0,0,1)' })
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+    }
+},
+{
+    group: getTranslation('clinic'),
+    title: getTranslation('clinic'),
+    query: "[out:json][timeout:25];(nwr[\"amenity\"=\"clinic\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var fill = new ol.style.Fill({
+            color: 'rgba(0,255,0,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(0,255,0,1)',
+            width: 1
+        });
+        return new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale: 0.0200
+            }),
+            text: new ol.style.Text({
+                text: feature.get(\"name\") || '',
+                offsetX: 7,
+                offsetY: -12,
+                fill: new ol.style.Fill({ color: 'rgba(0,0,0,1)' })
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+    }
+},
+{
+    group: getTranslation('doctor'),
+    title: getTranslation('doctor'),
+    query: "[out:json][timeout:25];(nwr[\"amenity\"=\"doctors\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var fill = new ol.style.Fill({
+            color: 'rgba(0,255,0,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(0,255,0,1)',
+            width: 1
+        });
+        return new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale: 0.0200
+            }),
+            text: new ol.style.Text({
+                text: feature.get(\"name\") || '',
+                offsetX: 7,
+                offsetY: -12,
+                fill: new ol.style.Fill({ color: 'rgba(0,0,0,1)' })
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+    }
+},
+{
+    group: getTranslation('dentist'),
+    title: getTranslation('dentist'),
+    query: "[out:json][timeout:25];(nwr[\"amenity\"=\"dentist\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var fill = new ol.style.Fill({
+            color: 'rgba(0,255,0,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(0,255,0,1)',
+            width: 1
+        });
+        return new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale: 0.0200
+            }),
+            text: new ol.style.Text({
+                text: feature.get(\"name\") || '',
+                offsetX: 7,
+                offsetY: -12,
+                fill: new ol.style.Fill({ color: 'rgba(0,0,0,1)' })
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+    }
+},
+{
+    group: getTranslation('laboratory'),
+    title: getTranslation('laboratory'),
+    query: "[out:json][timeout:25];(nwr[\"amenity\"=\"laboratory\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var fill = new ol.style.Fill({
+            color: 'rgba(0,255,0,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(0,255,0,1)',
+            width: 1
+        });
+        return new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale: 0.0200
+            }),
+            text: new ol.style.Text({
+                text: feature.get(\"name\") || '',
+                offsetX: 7,
+                offsetY: -12,
+                fill: new ol.style.Fill({ color: 'rgba(0,0,0,1)' })
+            }),
+            fill: fill,
+            stroke: stroke
+        });
     }
 },
 {
