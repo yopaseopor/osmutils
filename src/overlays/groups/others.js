a65447907b3e7c70919f98f3f4a9c69e1dd314c8
yopaseopor <yopaseopor@gmail.com>
2025-07-24 01:43:47 +0200

    Update others.js

--- src/overlays/groups/others.js
+++ src/overlays/groups/others.js
@@ -4469,6 +4469,42 @@ export function othersOverlays() {
 },
 {
     group: getTranslation('model'),
+    title: getTranslation('model'),
+    query: "[out:json][timeout:25];(nwr[\"shop\"=\"model\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var key_regex = /^name$/
+        var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+        var name = feature.get(name_key) || '';
+        var fill = new ol.style.Fill({
+            color: 'rgba(240,128,128,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(240,128,128,1)',
+            width: 1
+        });
+        var style = new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale:0.30
+            }),
+            text: new ol.style.Text({
+                text: name,
+                offsetX : 7,
+                offsetY : -12,
+                fill: new ol.style.Fill({
+                    color: 'rgba(0,0,0,1)'
+                }),
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+        return style;
+    }
+},
+{
+    group: getTranslation('model'),
 	title: "\u30bf\u30e0\u30bf\u30e0",
     query: "[out:json][timeout:25];(nwr[\"brand\"=\"\u30bf\u30e0\u30bf\u30e0\"][\"brand:en\"=\"TamTam\"][\"brand:ja\"=\"\u30bf\u30e0\u30bf\u30e0\"][\"brand:wikidata\"=\"Q30926431\"][\"name\"=\"\u30bf\u30e0\u30bf\u30e0\"][\"name:en\"=\"TamTam\"][\"name:ja\"=\"\u30bf\u30e0\u30bf\u30e0\"][\"shop\"=\"model\"]({{bbox}});node(w););out meta;",
     iconSrc: "src/img/logos/generic.svg",
@@ -4518,6 +4554,42 @@ export function othersOverlays() {
         });
         var stroke = new ol.style.Stroke({
             color: 'rgba(255,0,0,1)',
+            width: 1
+        });
+        var style = new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale:0.30
+            }),
+            text: new ol.style.Text({
+                text: name,
+                offsetX : 7,
+                offsetY : -12,
+                fill: new ol.style.Fill({
+                    color: 'rgba(0,0,0,1)'
+                }),
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+        return style;
+    }
+},
+{
+    group: getTranslation('plant_hire'),
+    title: getTranslation('plant_hire'),
+    query: "[out:json][timeout:25];(nwr[\"shop\"=\"plant_hire\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var key_regex = /^name$/
+        var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+        var name = feature.get(name_key) || '';
+        var fill = new ol.style.Fill({
+            color: 'rgba(60,179,113,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(60,179,113,1)',
             width: 1
         });
         var style = new ol.style.Style({
