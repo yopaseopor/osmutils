5a35b4d261cd60ee65556213e8cb1e7730af85e7
yopaseopor <yopaseopor@gmail.com>
2025-07-23 16:05:31 +0200

    Update mobility.js

--- src/overlays/groups/mobility.js
+++ src/overlays/groups/mobility.js
@@ -50009,6 +50009,42 @@ export function mobilityOverlays() {
 },
 {
     group: getTranslation('car_parts'),
+    title: getTranslation('car_parts'),
+    query: "[out:json][timeout:25];(nwr[\"shop\"=\"car_parts\"]({{bbox}});node(w););out meta;",
+    iconSrc: "src/img/logos/generic.svg",
+    iconStyle: "background-color:rgba(255,255,255,0.4)",
+    style: function (feature) {
+        var key_regex = /^name$/
+        var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+        var name = feature.get(name_key) || '';
+        var fill = new ol.style.Fill({
+            color: 'rgba(128,0,128,0.4)'
+        });
+        var stroke = new ol.style.Stroke({
+            color: 'rgba(128,0,128,1)',
+            width: 1
+        });
+        var style = new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "src/img/logos/generic.svg",
+                scale:0.30
+            }),
+            text: new ol.style.Text({
+                text: name,
+                offsetX : 7,
+                offsetY : -12,
+                fill: new ol.style.Fill({
+                    color: 'rgba(0,0,0,1)'
+                }),
+            }),
+            fill: fill,
+            stroke: stroke
+        });
+        return style;
+    }
+},
+{
+    group: getTranslation('car_parts'),
 	title: "PV Automotive",
     query: "[out:json][timeout:25];(nwr[\"brand\"=\"PV Automotive\"][\"brand:wikidata\"=\"Q2044844\"][\"name\"=\"PV Automotive\"][\"shop\"=\"car_parts\"]({{bbox}});node(w););out meta;",
     iconSrc: "src/img/logos/generic.svg",
