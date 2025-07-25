b8e7ddb351dc4391c9c1344c27843c945ffc66a1
yopaseopor <yopaseopor@gmail.com>
2025-07-25 15:25:10 +0200

    Update shopping.js

--- src/overlays/groups/shopping.js
+++ src/overlays/groups/shopping.js
@@ -96953,44 +96953,7 @@ return style;
             stroke: stroke
         });
         return style;
-    }
-},
-{
-    group: getTranslation('supermarket'),
-    title: getTranslation('supermarket'),
-    query: "[out:json][timeout:25];(nwr[\"shop\"=\"supermarket\"]({{bbox}});node(w););out meta;",
-    iconSrc: "src/img/logos/generic.svg",
-    iconStyle: "background-color:rgba(255,255,255,0.4)",
-    style: function (feature) {
-        var key_regex = /^name$/
-        var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
-        var name = feature.get(name_key) || '';
-        var fill = new ol.style.Fill({
-            color: 'rgba(0,0,255,0.3)'
-        });
-        var stroke = new ol.style.Stroke({
-            color: 'rgba(0,0,255,0.8)',
-            width: 1
-        });
-        var style = new ol.style.Style({
-            image: new ol.style.Icon({
-                src: "src/img/logos/generic.svg",
-                scale: 0.25
-            }),
-            text: new ol.style.Text({
-                text: name,
-                offsetX: 7,
-                offsetY: -12,
-                fill: new ol.style.Fill({
-                    color: 'rgba(0,0,0,1)'
-                }),
-            }),
-            fill: fill,
-            stroke: stroke
-        });
-        return style;
-
-    		
+ 
             }
         }
     ];
