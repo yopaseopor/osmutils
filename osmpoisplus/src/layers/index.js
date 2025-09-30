fcb07f5a599a9b17a433f677644da51006bfa3d3
yopaseopor <yopaseopor@gmail.com>
2025-09-30 22:50:11 +0200

    Update index.js

--- osmpoisplus/src/layers/index.js
+++ osmpoisplus/src/layers/index.js
@@ -2,73 +2,38 @@
 // Example: import { myLayer } from './myLayer.js';
 // export const allLayers = { myLayer };
 
-import { ASLayers } from './generated/as_wms_layers.js';
-import { ATLayers } from './generated/at_wms_layers.js';
-import { AULayers } from './generated/au_wms_layers.js';
-import { BELayers } from './generated/be_wms_layers.js';
-import { BRLayers } from './generated/br_wms_layers.js';
-import { CALayers } from './generated/ca_wms_layers.js';
-import { CHLayers } from './generated/ch_wms_layers.js';
-import { CZLayers } from './generated/cz_wms_layers.js';
 import { DELayers } from './generated/de_wms_layers.js';
-import { DKLayers } from './generated/dk_wms_layers.js';
-import { EELayers } from './generated/ee_wms_layers.js';
 import { ESLayers } from './generated/es_wms_layers.js';
-import { FILayers } from './generated/fi_wms_layers.js';
-import { FMLayers } from './generated/fm_wms_layers.js';
-import { FRLayers } from './generated/fr_wms_layers.js';
 import { GBLayers } from './generated/gb_wms_layers.js';
-import { GULayers } from './generated/gu_wms_layers.js';
-import { HRLayers } from './generated/hr_wms_layers.js';
-import { HULayers } from './generated/hu_wms_layers.js';
-import { ITLayers } from './generated/it_wms_layers.js';
-import { LVLayers } from './generated/lv_wms_layers.js';
-import { MDLayers } from './generated/md_wms_layers.js';
-import { MHLayers } from './generated/mh_wms_layers.js';
-import { MPLayers } from './generated/mp_wms_layers.js';
-import { NOLayers } from './generated/no_wms_layers.js';
-import { NZLayers } from './generated/nz_wms_layers.js';
-import { PLLayers } from './generated/pl_wms_layers.js';
-import { PTLayers } from './generated/pt_wms_layers.js';
-import { PWLayers } from './generated/pw_wms_layers.js';
-import { SELayers } from './generated/se_wms_layers.js';
-import { SGLayers } from './generated/sg_wms_layers.js';
-import { TWLayers } from './generated/tw_wms_layers.js';
-import { USLayers } from './generated/us_wms_layers.js';
-import { UYLayers } from './generated/uy_wms_layers.js';
-import { XXLayers } from './generated/xx_wms_layers.js';
-import { ZALayers } from './generated/za_wms_layers.js';
-import { BE_TMS_LAYERS } from './generated/tms_layers_BE.js';
-import { BR_TMS_LAYERS } from './generated/tms_layers_BR.js';
-import { CZ_TMS_LAYERS } from './generated/tms_layers_CZ.js';
-import { DK_TMS_LAYERS } from './generated/tms_layers_DK.js';
-import { FI_TMS_LAYERS } from './generated/tms_layers_FI.js';
-import { HT_TMS_LAYERS } from './generated/tms_layers_HT.js';
-import { HU_TMS_LAYERS } from './generated/tms_layers_HU.js';
-import { IT_TMS_LAYERS } from './generated/tms_layers_IT.js';
-import { LT_TMS_LAYERS } from './generated/tms_layers_LT.js';
-import { LU_TMS_LAYERS } from './generated/tms_layers_LU.js';
-import { MD_TMS_LAYERS } from './generated/tms_layers_MD.js';
-import { NL_TMS_LAYERS } from './generated/tms_layers_NL.js';
-import { NZ_TMS_LAYERS } from './generated/tms_layers_NZ.js';
-import { PH_TMS_LAYERS } from './generated/tms_layers_PH.js';
-import { PL_TMS_LAYERS } from './generated/tms_layers_PL.js';
-import { RS_TMS_LAYERS } from './generated/tms_layers_RS.js';
-import { SE_TMS_LAYERS } from './generated/tms_layers_SE.js';
-import { SG_TMS_LAYERS } from './generated/tms_layers_SG.js';
-import { SK_TMS_LAYERS } from './generated/tms_layers_SK.js';
-import { TW_TMS_LAYERS } from './generated/tms_layers_TW.js';
-import { XX_TMS_LAYERS } from './generated/tms_layers_XX.js';
-import { ZA_TMS_LAYERS } from './generated/tms_layers_ZA.js';
-import { ZM_TMS_LAYERS } from './generated/tms_layers_ZM.js';
-import { myCustomLayer } from './myCustomLayer.js';
-import { osmBrightLayer } from './osmBrightLayer.js';
-import { stamenTonerLayer } from './stamenTonerLayer.js';
+//import { BE_TMS_LAYERS } from './generated/tms_layers_BE.js';
+//import { BR_TMS_LAYERS } from './generated/tms_layers_BR.js';
+//import { CZ_TMS_LAYERS } from './generated/tms_layers_CZ.js';
+//import { DK_TMS_LAYERS } from './generated/tms_layers_DK.js';
+//import { FI_TMS_LAYERS } from './generated/tms_layers_FI.js';
+//import { HT_TMS_LAYERS } from './generated/tms_layers_HT.js';
+//import { HU_TMS_LAYERS } from './generated/tms_layers_HU.js';
+//import { IT_TMS_LAYERS } from './generated/tms_layers_IT.js';
+//import { LT_TMS_LAYERS } from './generated/tms_layers_LT.js';
+//import { LU_TMS_LAYERS } from './generated/tms_layers_LU.js';
+//import { MD_TMS_LAYERS } from './generated/tms_layers_MD.js';
+//import { NL_TMS_LAYERS } from './generated/tms_layers_NL.js';
+//import { NZ_TMS_LAYERS } from './generated/tms_layers_NZ.js';
+//import { PH_TMS_LAYERS } from './generated/tms_layers_PH.js';
+//import { PL_TMS_LAYERS } from './generated/tms_layers_PL.js';
+//import { RS_TMS_LAYERS } from './generated/tms_layers_RS.js';
+//import { SE_TMS_LAYERS } from './generated/tms_layers_SE.js';
+//import { SG_TMS_LAYERS } from './generated/tms_layers_SG.js';
+//import { SK_TMS_LAYERS } from './generated/tms_layers_SK.js';
+//import { TW_TMS_LAYERS } from './generated/tms_layers_TW.js';
+//import { XX_TMS_LAYERS } from './generated/tms_layers_XX.js';
+//import { ZA_TMS_LAYERS } from './generated/tms_layers_ZA.js';
+//import { ZM_TMS_LAYERS } from './generated/tms_layers_ZM.js';
+//import { myCustomLayer } from './myCustomLayer.js';
+//import { osmBrightLayer } from './osmBrightLayer.js';
+//import { stamenTonerLayer } from './stamenTonerLayer.js';
 
-export const allLayers = { ASLayers, ATLayers, AULayers, BELayers, BRLayers, CALayers, CHLayers, CZLayers,
-  DELayers, DKLayers, EELayers, ESLayers, FILayers, FMLayers, FRLayers, GBLayers, GULayers,
-  HRLayers, HULayers, ITLayers, LVLayers, MDLayers, MHLayers, MPLayers, NOLayers, NZLayers,
-  PLLayers, PTLayers, PWLayers, SELayers, SGLayers, TWLayers, USLayers, UYLayers, XXLayers, ZALayers, 
-  BE_TMS_LAYERS, BR_TMS_LAYERS, CZ_TMS_LAYERS, DK_TMS_LAYERS, FI_TMS_LAYERS, HT_TMS_LAYERS, HU_TMS_LAYERS, IT_TMS_LAYERS, LT_TMS_LAYERS, LU_TMS_LAYERS, MD_TMS_LAYERS, NL_TMS_LAYERS, NZ_TMS_LAYERS, PH_TMS_LAYERS, PL_TMS_LAYERS, RS_TMS_LAYERS, SE_TMS_LAYERS, SG_TMS_LAYERS, SK_TMS_LAYERS, TW_TMS_LAYERS, XX_TMS_LAYERS, ZA_TMS_LAYERS, ZM_TMS_LAYERS,
-  myCustomLayer, osmBrightLayer, stamenTonerLayer 
+export const allLayers = {
+  DELayers,
+  ESLayers,
+  GBLayers
 };
