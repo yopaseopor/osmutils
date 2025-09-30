e4dd903aa63608ab11a4c4bac9022858b9e9d551
yopaseopor <yopaseopor@gmail.com>
2025-09-30 22:41:59 +0200

    test

--- osmpoisplus/src/overlays/external/loader.js
+++ osmpoisplus/src/overlays/external/loader.js
@@ -1,7 +1,7 @@
-import { foodOverlays } from './food.js';
-import { customOverlays } from './custom_overlays.js';
+//import { foodOverlays } from './food.js';
+//import { customOverlays } from './custom_overlays.js';
 
 // Function to load external overlay files
 export function loadExternalOverlays() {
-    return Promise.resolve([...foodOverlays, ...customOverlays]);
+    return Promise.resolve([]); // Return empty array as external overlays are no longer used
 } 
